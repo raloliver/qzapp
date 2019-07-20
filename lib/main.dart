44b732e74ef99d193b4a1937923888a27febcf6b
Ral Oliver <raloliver.ws@gmail.com>
2019-07-19 22:58:38 -0300

    Stack

--- lib/main.dart
+++ lib/main.dart
@@ -8,30 +8,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: Container(
-              color: Colors.blue,
-              padding: EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Icon(
-                    CupertinoIcons.game_controller,
-                    color: Colors.white,
-                    size: 50,
-                  ),
-                  Icon(
-                    CupertinoIcons.game_controller,
-                    color: Colors.white,
+        body: SizedBox.expand(
+          child: Stack(
+            children: <Widget>[
+              Icon(
+                Icons.camera,
+                size: 100,
+                color: Colors.red,
+              ),
+              Align(
+                  alignment: Alignment.center,
+                  child: Icon(
+                    Icons.camera,
                     size: 100,
-                  ),
-                  Icon(
-                    CupertinoIcons.game_controller,
-                    color: Colors.white,
-                    size: 200,
-                  ),
-                ],
-              ))),
+                    color: Colors.blue,
+                  )),
+              Positioned(
+                  bottom: 20,
+                  left: 100,
+                  child: Icon(
+                    Icons.camera,
+                    size: 100,
+                    color: Colors.green,
+                  ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
